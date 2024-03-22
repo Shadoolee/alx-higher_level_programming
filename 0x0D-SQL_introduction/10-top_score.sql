1ddd3c95b1f9da49a3fd2e47a2b194d51cd4955f
Shadoolee <oluwaseundvd@gmail.com>
2024-03-22 15:40:17 -0700

    SQL Introduction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all recors of the table second_table.
--- Recors are ordered by desending score.
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+-- Records are ordered by desending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
--- Lists all tables of a database in MySQL server.
-SHOW TABLE
+-- Lists all tables of a database on my MySQL server.
+SHOW TABLES;
